d095732bb5bd206dc6f9bea5bb53168203e5de30
guillaume.farge@efrei.net <spore.gui@hotmail.fr>
2020-03-23 01:20:15 +0100

    updtae changes to bot.py

--- bot.py
+++ bot.py
@@ -2,9 +2,17 @@
 import os
 import random
 
+import time
+import asyncio
+
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
+
+from player import Player
+from roles import *
+
+from target import Target
 
 
 load_dotenv()
@@ -25,10 +33,29 @@ HISTORY_TEXT_CHANNEL_NAME = "Histoire"
 
 PRIVATE_TEXT_CHANNEL_NAME = 'Ton role et actions'
 
+LOUPS_TEXT_CHANNEL_NAME = 'Loups Garous'
+
+TIME_FOR_LOUPS = 30
+
+TIME_FOR_ROLES = 10
+
+bot.GAME_CREATED = False
 bot.GAME_STARTED = False
 bot.PLAYERS = []
+bot.DEADS = []
+bot.LOUPS = []
+bot.ALIVE_PLAYERS = []
+bot.LOUP_TARGETS = []
 
-MINIMUM_PLAYER_NB = 1
+bot.LOUP_FINAL_TARGET = None
+
+bot.MINIMUM_PLAYER_NB = 4  # TODO: in prod : 4 players min
+
+bot.NB_LOUP = 2
+
+bot.NB_NIGHT = 1
+
+bot.TURN = ""
 
 
 # on ready function: when the bot connects to the server
@@ -38,16 +65,15 @@ async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
 
 
-
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.errors.CheckFailure):
         await ctx.send('You do not have the correct role for this command.')
     print(error)
-    #raise
+    # raise
 
 
-@bot.command(name='99',help='Responds with a random quote from Brooklyn 99')
+@bot.command(name='99', help='Responds with a random quote from Brooklyn 99')
 async def nine_nine(ctx):
     brooklyn_99_quotes = [
         'I\'m the human form of the 💯 emoji.',
@@ -61,6 +87,7 @@ async def nine_nine(ctx):
     response = random.choice(brooklyn_99_quotes)
     await ctx.send(response)
 
+
 @bot.command(name='roll_dice', help='Simulates rolling dice. !roll_dice <NB_OF_DICES> <NB_OF_SIDES>')
 async def roll(ctx, number_of_dice: int, number_of_sides: int):
     dice = [
@@ -69,7 +96,8 @@ async def roll(ctx, number_of_dice: int, number_of_sides: int):
     ]
     await ctx.send(', '.join(dice))
 
-@bot.command(name='create-channel',help='create a channel')
+
+@bot.command(name='create-channel', help='create a channel')
 @commands.has_role('admin')
 async def create_channel(ctx, channel_name='real-python'):
     guild = ctx.guild
@@ -79,78 +107,318 @@ async def create_channel(ctx, channel_name='real-python'):
         await guild.create_text_channel(channel_name)
 
 
-@bot.command(name='new',help='crée une nouvelle partie')
+@bot.command(name='new', help='crée une nouvelle partie')
 @commands.has_role(MASTER_OF_THE_GAME)
 async def new_game(ctx):
-    if(bot.GAME_STARTED == False):
-        await ctx.send('Nouvelle partie créée, utiliser la commande !join pour rejoindre la partie')
-        bot.GAME_STARTED = True
-    else:
-        await ctx.send('Une partie est en cours')
+    if(bot.GAME_CREATED == True):
+        await ctx.send('Une partie est déjà créée')
+        return
+    if(bot.GAME_STARTED == True):
+        await ctx.send('Une partie est déjà en cours')
+        return
 
-@bot.command(name='join',help='rejoindre une partie')
-#@commands.has_role(MASTER_OF_THE_GAME)
+    bot.PLAYERS.append(Player(ctx.author))
+    nameList = [player.discordMember.display_name for player in bot.PLAYERS]
+    bot.GAME_CREATED = True
+
+    message = 'Nouvelle partie créée, utiliser la commande !join pour rejoindre la partie\n\n'
+    message += f'{ctx.author.display_name} a rejoint la partie\n\n'
+    message += f'joueurs: {nameList}\n'
+    await ctx.send(message)
+
+    # save the channel where the new was typed
+    bot.BEGINNING_CHANNEL = ctx.message.channel
+
+
+@bot.command(name='join', help='rejoindre une partie')
+# @commands.has_role(MASTER_OF_THE_GAME)
 async def join_game(ctx):
     name = ctx.author.display_name
     print(f'{name} tried to joined')
-    if(bot.GAME_STARTED == True):
-        #print(ctx.__dict__)
-        if ctx.author not in bot.PLAYERS:
-            bot.PLAYERS.append(ctx.author)
-            
-            await ctx.send(f'{name} a rejoint la partie')
-            nameList = []
-            for player in bot.PLAYERS:
-                nameList.append(player.display_name)
-            await ctx.send(f'joueurs: {nameList}')
-        else:
-            await ctx.send(f'{name} vous êtes déjà dans la partie')
-        print(nameList)
 
-        #if(bot.PLAYERS):
-    else:
+    if(ctx.message.channel != bot.BEGINNING_CHANNEL):
+        await ctx.send('message envoyé dans le mauvais channel')
+        return
+
+    if(bot.GAME_CREATED == False):
         await ctx.send(f'aucune partie en cours')
-    
+        return
 
-@bot.command(name='stop',help='stop la partie en cours')
+    if(bot.GAME_STARTED == True):
+        await ctx.send(f'la partie a déjà commencé')
+        return
+
+    print(bot.GAME_CREATED, bot.GAME_STARTED)
+
+    # print(ctx.__dict__)
+    memberList = [player.discordMember for player in bot.PLAYERS]
+    if ctx.author in memberList:
+        await ctx.send(f'{name} vous êtes déjà dans la partie')
+        return
+
+    bot.PLAYERS.append(Player(ctx.author))
+    nameList = [
+        player.discordMember.display_name for player in bot.PLAYERS]
+
+    roles = assign_roles()
+    print(roles)
+
+    message = f'{name} a rejoint la partie\n\n'
+    message += f'joueurs: {nameList}\n\n'
+    message += f'{roles}\n'
+    await ctx.send(message)
+
+    print(nameList)
+
+
+@bot.command(name='stop', help='stop la partie en cours')
 @commands.has_role(MASTER_OF_THE_GAME)
 async def stop_game(ctx):
-    if(bot.GAME_STARTED == True):
-        await ctx.send('arret de la partie en cours')
-        await delete_game_category(ctx)
-        bot.GAME_STARTED = False
-        bot.PLAYERS.clear()
-    else:
+    if(bot.GAME_CREATED == False):
         await ctx.send('aucune partie en cours')
-    
+        return
 
-@bot.command(name='start',help='commence la partie avec toutes les personnes ayant effectué !join')
+    await ctx.send('arret de la partie en cours')
+    await delete_game_category(ctx)
+    bot.GAME_CREATED = False
+    bot.GAME_STARTED = False
+    bot.PLAYERS.clear()
+    bot.ALIVE_PLAYERS.clear()
+    bot.LOUPS.clear()
+    bot.LOUP_TARGETS.clear()
+    bot.DEADS.clear()
+
+
+@bot.command(name='delete', help="supprime les categrory du jeu si aucune partie n'est en cours")
+@commands.has_role(MASTER_OF_THE_GAME)
+async def delete_channels(ctx):
+    if(bot.GAME_CREATED == True):
+        await ctx.send('une partie a été créée')
+        return
+    if(bot.GAME_STARTED == True):
+        await ctx.send('une partie est en cours')
+        return
+
+    await ctx.send('suppression des channels de jeu')
+    await delete_game_category(ctx)
+
+
+@bot.command(name='start', help='commence la partie avec toutes les personnes ayant effectuées !join')
 @commands.has_role(MASTER_OF_THE_GAME)
 async def start_game(ctx):
-    if(bot.GAME_STARTED == True):
-        if(len(bot.PLAYERS) >= MINIMUM_PLAYER_NB):
-            await ctx.send('la partie commence!')
-            await create_game_category(ctx)
-            guild = ctx.guild
-            game_voice_channel = bot.GAME_VOICE_CHANNEL
-
-            # move the player to the game voice channel
-            for player in bot.PLAYERS:
-                print(player)
-                try:
-                    await player.move_to(game_voice_channel)
-                except Exception as e:
-                    print(e)
-        else:
-            await ctx.send(f"le nombre minimum de joueurs ({MINIMUM_PLAYER_NB}) n'est pas atteint")
-    else:
+    if(bot.GAME_CREATED == False):
         await ctx.send('aucune partie créée')
+        return
 
-    #beginningChannel = None
+    if(len(bot.PLAYERS) < bot.MINIMUM_PLAYER_NB):
+        await ctx.send(f"le nombre minimum de joueurs ({bot.MINIMUM_PLAYER_NB}) n'est pas atteint")
+        return
+
+    await ctx.send('la partie commence!')
+    bot.GAME_STARTED = True
+    await create_game_category(ctx)
+    guild = ctx.guild
+    game_voice_channel = bot.GAME_VOICE_CHANNEL
+
+    # move the player to the game voice channel
+    for player in bot.PLAYERS:
+        member = player.discordMember
+        print(member.display_name)
+        try:
+            await member.move_to(game_voice_channel)
+        except Exception as e:
+            print(e)
+
+    await game_process(ctx)
+
+    # beginningChannel = None
     # search for the beginning voice channel
-    #discord.utils.get(guild.categories, name=GAME_CATEGORY_NAME)
-    #print(beginningChannel.members)
+    # discord.utils.get(guild.categories, name=GAME_CATEGORY_NAME)
+    # print(beginningChannel.members)
 
+
+async def game_process(ctx):
+    await bot.HISTORY_TEXT_CHANNEL.send(
+        'vous avez 10 secondes avant que la partie commence')
+    bot.TURN = "ROLES"
+    # wait the time that the people check their roles
+    await asyncio.sleep(TIME_FOR_ROLES)
+    # while(not someone win or draw)
+
+    await bot.HISTORY_TEXT_CHANNEL.send(f'la nuit {bot.NB_NIGHT} tombe')
+
+    ### wake loups ###
+    await loups_turn()
+
+    ### wake villageois ###
+    await bot.HISTORY_TEXT_CHANNEL.send('Le village se réveille')
+    bot.TURN = "DAY"
+
+    ### compute deads ###
+    bot.DEADS.append(bot.LOUP_FINAL_TARGET)
+
+    ### display the night deads ###
+    message = ""
+    for player in bot.DEADS:
+        message += f"{player.discordMember.display_name} est mort ce soir\n"
+    await bot.HISTORY_TEXT_CHANNEL.send(message)
+
+    ### remove dead players from alive table ###
+    index = 0
+    for player in bot.DEADS:
+        if(player in bot.ALIVE_PLAYERS):
+            # TODO: remove from ALIVE_PLAYERS table
+            pass
+        index += 1
+    # the loup final_target can be reset to None
+    bot.LOUP_FINAL_TARGET = None
+
+    ### vote for maire ###
+    bot.TURN = "ELECTION"
+
+    ### vote for day kill ###
+    bot.TURN = "LINCHAGE"
+
+    ### display kill of the day ###
+
+    ### check someone wins or draw ###
+
+    bot.NB_NIGHT += 1
+
+
+@bot.command(name='vote', help='vote pendant la partie')
+# @commands.has_role(MASTER_OF_THE_GAME)
+async def vote(ctx, choice: int):
+    if(bot.GAME_CREATED == False):
+        await ctx.send('aucune partie créée')
+        return
+    if(bot.GAME_STARTED == False):
+        await ctx.send("la partie n'a pas commencé")
+        return
+
+    # if vote during TURN="LOUPS"
+    if(bot.TURN == "LOUPS"):
+        # if vote dans channel loup
+        if(ctx.message.channel == bot.LOUPS_TEXT_CHANNEL):
+            # if choice is valid
+            if(choice >= 1 and choice <= len(bot.ALIVE_PLAYERS)):
+                targets = [num for target.actual_num in bot.LOUP_TARGETS]
+                # if the choice is not already a target
+                if(choice not in targets):
+                    target = Target(
+                        choice, bot.ALIVE_PLAYERS[choice], ctx.message.author)
+                    bot.LOUP_TARGETS.append(target)
+                else:
+                    # else we add the new accusator to the list
+                    current_target = None
+                    for target in bot.LOUP_TARGETS:
+                        if(target.actual_num == choice):
+                            current_target = target
+                            break
+                    if(current_target == None):
+                        raise IndexError
+                    current_target.add_accusator(ctx.message.author)
+
+                await bot.LOUPS_TEXT_CHANNEL.send(f"{bot.LOUP_TARGETS}")
+
+
+async def loups_turn():
+
+    # warn village
+    await bot.HISTORY_TEXT_CHANNEL.send(f'Les loups garous ont 20 secondes pour choisir leur victime de la nuit:\n\n')
+    bot.TURN = "LOUPS"
+
+    # make channel accessible
+    for player in bot.LOUPS:
+        member = player.discordMember
+        #overwrites[member] = discord.PermissionOverwrite(read_messages=True)
+        await bot.LOUPS_TEXT_CHANNEL.set_permissions(
+            member, send_messages=True, read_messages=True)
+
+    # warn loups garou
+    message = f'Vous avez {TIME_FOR_LOUPS} secondes pour choisir votre victime de la nuit:\n\n'
+    time_left = TIME_FOR_LOUPS
+
+    num = 1
+    for player in bot.ALIVE_PLAYERS:
+        member = player.discordMember
+        message += f'{num}:  {member.display_name}\n'
+        num += 1
+    message += 'commande: !vote <int>\n'
+    message += 'exemple: !vote 5\n'
+    await bot.LOUPS_TEXT_CHANNEL.send(message)
+
+    await asyncio.sleep(time_left - 10)
+    time_left = 10
+    await bot.LOUPS_TEXT_CHANNEL.send('10 secondes restantes')
+    await asyncio.sleep(time_left - 5)
+    time_left = 5
+    await bot.LOUPS_TEXT_CHANNEL.send('5 secondes restantes')
+    await asyncio.sleep(time_left)
+
+    # find the target:
+
+    # if no target
+    if(len(bot.LOUP_TARGETS) == 0):
+        # no dead from the loups
+        await bot.LOUPS_TEXT_CHANNEL.send("vous n'avez pas choisi de victime")
+    else:
+        targets_choice = None
+        max_accusator = max([len(target.accusator)
+                             for target in bot.LOUP_TARGETS])
+        targets_choice = [target for target in bot.LOUP_TARGETS if len(
+            target.accusator) == max_accusator]
+
+        # draw for the votes
+        if(len(targets_choice) > 1):
+            # choose the target randomly
+            rand_index = random.randint(0, len(targets_choice) - 1)
+            target_choice = targets_choice[rand_index]
+        elif(len(targets_choice == 1)):
+            target_choice = targets_choice[0]
+        else:
+            raise Exception
+
+        # warn loups of the choice
+        await bot.LOUPS_TEXT_CHANNEL.send(f'votre choix est fait, vous avez choisi {target_choice}')
+        bot.LOUP_FINAL_TARGET = target_choice.player
+        bot.LOUP_TARGETS.clear()
+
+    # make channel inacessible
+    for player in bot.LOUPS:
+        member = player.discordMember
+        #overwrites[member] = discord.PermissionOverwrite(read_messages=True)
+        await bot.LOUPS_TEXT_CHANNEL.set_permissions(
+            member, send_messages=False, read_messages=False)
+
+
+@bot.command(name='loup', help='configure le nombre de loups pour la partie')
+@commands.has_role(MASTER_OF_THE_GAME)
+async def assign_nb_loup(ctx, number_of_loups: int):
+    if(bot.GAME_CREATED == False):
+        await ctx.send('aucune partie créée')
+        return
+    if(bot.GAME_STARTED == True):
+        await ctx.send('la partie a déjà commencé')
+        return
+
+    bot.NB_LOUP = number_of_loups
+    await ctx.send(f'nombre de loups:{bot.NB_LOUP}')
+    # TODO: Reassign roles
+
+
+@bot.command(name='min-players', help='configure le nombre de joueurs minimums pour la partie')
+@commands.has_role(MASTER_OF_THE_GAME)
+async def assign_min_players(ctx, min_number_of_players: int):
+    if(bot.GAME_CREATED == False):
+        await ctx.send('aucune partie créée')
+        return
+    if(bot.GAME_STARTED == True):
+        await ctx.send('la partie a déjà commencé')
+        return
+
+    bot.MINIMUM_PLAYER_NB = min_number_of_players
+    await ctx.send(f'nombre de joueurs minimum: {bot.MINIMUM_PLAYER_NB}')
 
 
 async def create_game_category(ctx):
@@ -158,36 +426,92 @@ async def create_game_category(ctx):
     guild = ctx.guild
 
     # create the game category, it will overwrite an existing one
-    category_exists = discord.utils.get(guild.categories, name=GAME_CATEGORY_NAME)
+    category_exists = discord.utils.get(
+        guild.categories, name=GAME_CATEGORY_NAME)
     if category_exists:
         await delete_game_category(ctx)
     # create a category for the game
     game_category = await guild.create_category(GAME_CATEGORY_NAME)
     bot.GAME_CATEGORY = game_category
     print(f'Creating a new category: {GAME_CATEGORY_NAME}')
-    
+
     # create the history text channel
     bot.HISTORY_TEXT_CHANNEL = await game_category.create_text_channel(name=HISTORY_TEXT_CHANNEL_NAME)
 
     # create the game voice channel
     bot.GAME_VOICE_CHANNEL = await game_category.create_voice_channel(name=GAME_VOICE_CHANNEL_NAME)
 
-    # TODO: attribute roles to all the people register in the game
+    overwrites = {
+        guild.default_role: discord.PermissionOverwrite(read_messages=False)
+    }
+
+    # for player in bot.LOUPS:
+    #    member = player.discordMember
+    #    overwrites[member] = discord.PermissionOverwrite(read_messages=True)
+
+    # TODO: create loup garou only channel
+    bot.LOUPS_TEXT_CHANNEL = await game_category.create_text_channel(LOUPS_TEXT_CHANNEL_NAME, overwrites=overwrites)
 
     # for each player create a secret text channel
     for player in bot.PLAYERS:
-        #print(player.guild_permissions.administrator)
-        #member = discord.utils.get(guild.members, id=player.id)
+        member = player.discordMember
+        # print(player.guild_permissions.administrator)
+        # member = discord.utils.get(guild.members, id=player.id)
         overwrites = {
             guild.default_role: discord.PermissionOverwrite(read_messages=False),
-            player: discord.PermissionOverwrite(read_messages=True)
+            member: discord.PermissionOverwrite(read_messages=True)
         }
-        if(player.guild_permissions.administrator):
-            private_channel = await game_category.create_text_channel(PRIVATE_TEXT_CHANNEL_NAME + "_admin_" + player.display_name, overwrites=overwrites)
+
+        if(member.guild_permissions.administrator):
+            player.private_channel = await game_category.create_text_channel(PRIVATE_TEXT_CHANNEL_NAME + "_admin_" + member.display_name, overwrites=overwrites)
         else:
-            private_channel = await game_category.create_text_channel(PRIVATE_TEXT_CHANNEL_NAME, overwrites=overwrites)
-        #setattr(player,'PRIVATE_CHANNEL', private_channel)
-        await private_channel.send(player.name)
+            player.private_channel = await game_category.create_text_channel(PRIVATE_TEXT_CHANNEL_NAME, overwrites=overwrites)
+        # setattr(player,'PRIVATE_CHANNEL', private_channel)
+        await player.private_channel.send(player.role)
+
+
+def assign_roles():
+    nb_players = len(bot.PLAYERS)
+    if(bot.NB_LOUP < nb_players):
+        nb_loup = bot.NB_LOUP
+    else:
+        nb_loup = int(nb_players/4)
+        if nb_loup == 0:
+            nb_loup = 1
+    # TODO: here add the number of the special roles
+
+    roles = []
+
+    for _ in range(nb_loup):
+        roles.append(LoupGarou())
+
+    # TODO: Here add the substraction of the nb of special role
+    nb_villageois = nb_players - nb_loup
+
+    for _ in range(nb_villageois):
+        roles.append(Villageois())
+
+    if(len(roles) != nb_players):
+        print('error nb_roles != nb_players')
+        raise Exception
+
+    random.shuffle(roles)
+
+    bot.LOUPS.clear()
+    bot.ALIVE_PLAYERS.clear()
+    for player, role in zip(bot.PLAYERS, roles):
+        player.role = role
+        if(isinstance(role, LoupGarou)):
+            bot.LOUPS.append(player)
+        # all players are now alive
+        bot.ALIVE_PLAYERS.append(player)
+
+    message = f"nombre de joueurs : {nb_players}\n"
+    message += f"loups : {nb_loup}\n"
+    message += f"villageois : {nb_villageois}\n"
+
+    return message
+
 
 async def delete_game_category(ctx):
     print(f'delete existing category {GAME_CATEGORY_NAME}')
@@ -196,7 +520,8 @@ async def delete_game_category(ctx):
     guild = ctx.guild
 
     # delete all channels before deleting the category
-    category_exists = discord.utils.get(guild.categories, name=GAME_CATEGORY_NAME)
+    category_exists = discord.utils.get(
+        guild.categories, name=GAME_CATEGORY_NAME)
     print(category_exists)
     if category_exists:
         for channel in category_exists.channels:
